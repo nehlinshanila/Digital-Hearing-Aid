63c0bcb38cf844aaa5705d414941cfc3e8366dbb
ZahinSam1 <toky.jahin@gmail.com>
2023-04-04 02:59:06 +0600

    fixed the loudness check

--- test/test/test2.py
+++ test/test/test2.py
@@ -5,30 +5,31 @@ import numpy as np
 chunk = 1024  # samples per frame
 channels = 2  # stereo
 sample_rate = 44100  # samples per second
-record_seconds = 5  # duration of recording
+record_seconds = 10  # duration of recording
 filename = "recording.wav"
 threshold = 0.4
 
 p = pyaudio.PyAudio()
 
-def callback(in_data, frame_count, time_info, status):
-    
-    return (in_data, pyaudio.paContinue)
-
 # open audio stream
-stream = p.open(format=pyaudio.paInt16, channels=channels, rate=sample_rate,
-                input=True, output=True, frames_per_buffer=chunk)
+stream = p.open(format=pyaudio.paInt16, 
+                channels=channels, 
+                rate=sample_rate,
+                input=True,
+                frames_per_buffer=chunk)
 
 # start recording
 frames = []
 for i in range(0, int(sample_rate / chunk * record_seconds)):
     data = stream.read(chunk)
-    samples = np.frombuffer(data, dtype=np.float32)
+    
+    samples = np.frombuffer(data, dtype=np.int16)
     # calculate root-mean-square (RMS) amplitude of samples
-    rms = np.sqrt(np.mean(samples**2))
-    # if rms < threshold:
+    # rms = np.sqrt(np.mean(samples**2))
+    rms =np.abs(np.sqrt(np.mean(np.square(samples/32767)))) 
     print(rms)
-    frames.append(data)
+    if rms < threshold:  
+        frames.append(data)
 
 # stop recording
 stream.stop_stream()
--- test/test/test3.py
+++ test/test/test3.py
@@ -1,19 +1,6 @@
-import numpy as np
 import sounddevice as sd
 
-# Define a scaling factor for the audio input
-scaling_factor = 0.5
-
-# Define a callback function to modify the audio input
-def audio_callback(indata, frames, time, status):
-    # Check if the audio input is valid
-    if status:
-        print(status, flush=True)
-    # Modify the audio input by scaling the audio signal
-    indata *= scaling_factor
-    # Play the modified audio input
-    sd.play(indata)
-
-# Start the audio input stream with the modified audio callback function
-with sd.InputStream(callback=audio_callback):
-    sd.sleep(10000)
+print(sd.query_devices())
+supported_formats = sd.query_hostapis()
+print("Supported input formats:", supported_formats['input'])
+print("Supported output formats:", supported_formats['output'])
